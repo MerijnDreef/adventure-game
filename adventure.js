33a76664fa14ded7ac7e3f528fea523de6efb36f
Merijn <99054535@mydavinci.nl>
2019-11-26 12:17:58 +0100

    buttons en text gedaan
    text

--- adventure.js
+++ adventure.js
@@ -3,25 +3,32 @@ document.body.appendChild(test);
 test.innerHTML = "Start";
 test.onclick = kaasRoll
 
+var title = document.getElementById('title');
+var text = document.getElementById('description');
+
 function kaasRoll(){
     background(0)
     Text(0)
 }
 function background(backgrOption){
     var ground = [
-        "url('css/images/dungeon-entrance-size.png')"
+        "url('css/images/dungeon-entrance-size.png')",
+        "url('css/images/dungeon-normal-room-size.png')"
     ];
     document.body.style.background = ground[backgrOption];
 }
+Math.floor(Math.random() * 3)
 function Text(){
-    var writing = document.createElement('h1');
-    var written = document.createElement('p');
-    
-    writing.innerText = 'The quest for Liberty';
-    writing.style.color = 'white';
-    document.body.appendChild(writing);
-
-    written.innerText = 'you are a warrior looking for a dungeon you have no party members because you do not want to lose them. So you are looking and looking you find a quest to slay a dungeon keeper who terrorises villages by plundering and murdering. His name? Bleeding Meteor noone knows how he looks, they only know that he is dangerous. As you go through the howling forest you see the dungeon, you enter through the stone doorway followed by a long hallway of torches it seems to be endless, but then you see an end,' + ' You are now in a room where an undead warrior awaits you, (you now go in to combat with the undead warrior - garuanteed drop key to next room)';
-    written.style.color = 'white';
-    document.body.appendChild(written);
+    document.getElementById("game-container").style.display = "block";
+    title.style.color = 'white';
+    text.style.color = 'white';
+    title.innerText = 'The quest for Liberty'
+    text.innerText = 'you are a warrior looking for a dungeon you have no party members because you do not want to lose them. So you are looking and looking you find a quest to slay a dungeon keeper who terrorises villages by plundering and murdering. His name? Bleeding Meteor noone knows how he looks, they only know that he is dangerous.'+'As you go through the howling forest you see the dungeon, you enter through the stone doorway followed by a long hallway of torches it seems to be endless, but then you see an end, You are now in a room where an undead warrior awaits you'
+    test.onclick = entrance
+}
+function entrance(){
+    document.getElementById("title").style.display = "none";
+    document.getElementById("description").style.display = "none";
+    background(1)
+    var enemy = document.createElement("img")
 }
